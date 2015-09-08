bd3b9c5b1335189029b89890056517faaa0c2779
DeeLiten <deeliten@gmail.com>
2015-09-08 11:26:36 +0200

    PL: change in user

--- LexBlog/LexBlog/Models/User.cs
+++ LexBlog/LexBlog/Models/User.cs
@@ -10,7 +10,7 @@ namespace LexBlog.Models
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int Email { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
         public DateTime Created { get; set; }
         public DateTime Edited { get; set; }
