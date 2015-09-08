97ce653d0e115df7b82cd796cade6c2e0c936e2a
DeeLiten <deeliten@gmail.com>
2015-09-08 11:25:30 +0200

    PL: user och role class

---
+++ LexBlog/LexBlog/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LexBlog.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Email { get; set; }
+        public string Password { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Edited { get; set; }
+        public string UserName { get; set; }
+        public int RoleId { get; set; }
+    }
+}
