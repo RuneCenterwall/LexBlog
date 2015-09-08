c8a9309b0c9ebf39423a8ae0ec4cdf78e1a8a48e
Andreas Magell <andreas.magell@outlook.com>
2015-09-08 11:27:29 +0200

    Comments and Post classes

---
+++ LexBlog/LexBlog/Models/Comment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LexBlog.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Body { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Edited { get; set; }
+    }
+}
