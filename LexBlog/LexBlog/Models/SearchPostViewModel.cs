f16105614a829141a429debfc5528884551adee6
HiNicken <hinicken@outlook.com>
2015-09-24 13:41:22 +0200

    NH: SearchPostModel uploaded (again...)

---
+++ LexBlog/LexBlog/Models/SearchPostViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LexBlog.Models
+{
+    public class SearchResultViewModel
+    {
+        public string SearchString { get; set; }
+        public List<Post> Posts { get; set; }
+        public List<Blog> Blogs { get; set; }
+    }
+}
