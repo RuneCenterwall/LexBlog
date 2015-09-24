333a4ff565f05cc093bf76f055ea6a9fd90c1c67
RuneCenterwall <rune.centerwall@gmail.com>
2015-09-24 14:53:21 +0200

    RC: Seed partially finished.

--- LexBlog/LexBlog/Migrations/Configuration.cs
+++ LexBlog/LexBlog/Migrations/Configuration.cs
@@ -20,24 +20,55 @@ namespace LexBlog.Migrations
             var store = new UserStore<ApplicationUser>(context);
             var manager = new UserManager<ApplicationUser>(store);
 
-            ApplicationUser user1 = new ApplicationUser {
-                UserName = "Per",
-                Email = "per@per.biz"
+            ApplicationUser user1 = new ApplicationUser
+            {
+                UserName = "AndersA",
+                Email = "anders.andersson@amail.com",
+                FirstName = "Anders",
+                LastName = "Andersson",
+                PhoneNumber = "+461123456",
             };
 
-            manager.Create(user1, "Pelle");
+            ApplicationUser user2 = new ApplicationUser
+            {
+                UserName = "BjörnB",
+                Email = "bjorn.bjornsson@amail.com",
+                FirstName = "Björn",
+                LastName = "Björnsson",
+                PhoneNumber = "+462123456",
+            };
 
+            ApplicationUser user3 = new ApplicationUser
+            {
+                UserName = "CarlC",
+                Email = "carl.carlsson@amail.com",
+                FirstName = "Carl",
+                LastName = "Carlsson",
+                PhoneNumber = "+463123456",
+            };
+
+            manager.Create(user1, "Aaa-123");
+            manager.Create(user2, "Bbb-123");
+            manager.Create(user3, "Ccc-123");
+
+            //RC: Rest not finished ...
             context.Blogs.AddOrUpdate(
                     b => b.Title, // identifier - if this value already exists, update instead of add
                     new LexBlog.Models.Blog()
                     {
                         Title = "TestBlog01",
-                        Description = "TestBody01",
+                        Description = "TestDescr01",
                         Created = DateTime.Now,
                         Edited = DateTime.Now,
-                        Views = 200
+                        Views = 10
+                        Owner = manager.Users.First<context>
+                        {
+
+                    }
+
                     }
                 );
+
         }
     }
 }
